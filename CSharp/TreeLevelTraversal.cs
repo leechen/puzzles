8197ec1b0a699bb753369fab0ad2fe4cd3b607ee
Liqiang Chen <leeh.chen@gmail.com>
2024-06-22 21:57:02 -0700

    add two tree questions

---
+++ CSharp/TreeLevelTraversal.cs
@@ -0,0 +1,44 @@
+/** https://leetcode.com/problems/binary-tree-level-order-traversal/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+
+        var res = new List<IList<int>>();
+        if (root == null) return res;
+        
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Any()) {
+            var level = new List<int>();
+            int len = queue.Count;
+
+            for (int i=0; i<len; i++) {
+                var curNode = queue.Dequeue();
+                level.Add(curNode.val);
+
+                if (curNode.left != null) {
+                    queue.Enqueue(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.Enqueue(curNode.right);
+                }                
+            }
+            res.Add(level);
+        }
+        
+        return res;
+    }
+}
---
+++ CSharp/TreeNodeDistance.cs
@@ -0,0 +1,61 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lintcode
+{
+    class Solution {
+        /**
+         * @param root: The root node of a binary tree.
+         * @param p: The value of a node in the binary tree.
+         * @param q: The value of a node in the binary tree.
+         * @return: The distance between two nodes in a binary tree.
+         */
+        public int CalculateDistance(TreeNode root, int p, int q) {
+            var lca = GetLCA(root, p, q);
+
+            int distToP = CalDist(lca, p, 0);
+            int distToQ = CalDist(lca, q, 0);
+            return distToP + distToQ;
+        }
+
+        private int CalDist(TreeNode root, int p, int dist) {
+            if (root == null) {
+                return Int32.MaxValue;
+            }
+            if (root.val == p) {
+                return dist;
+            }
+
+            return Math.Min(CalDist(root.left, p, dist+1), CalDist(root.right, p, dist+1));
+        }
+
+        /// Get least common acesstors
+        private TreeNode GetLCA(TreeNode root, int p, int q) {
+            if (root == null) {return null;}
+
+            if (root.val == p || root.val == q) {
+                return root;
+            }
+            var left = GetLCA(root.left, p, q);
+            var right = GetLCA(root.right, p, q);
+
+            if (left != null && right != null) {
+                return root;
+            }
+            return left == null ? right : left;
+        }
+    }
+}
