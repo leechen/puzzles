abf3ee96149cdb167980c3d3c8af836c66598e1c
Liqiang Chen <leeh.chen@gmail.com>
2024-06-28 21:53:22 -0700

    add another solution for max island size

--- python/maxAreaOfIslands.py
+++ python/maxAreaOfIslands.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
@@ -24,3 +23,27 @@ class Solution:
 
         return max_area
         
+
+class Solution2:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (
+                r < 0
+                or r == ROWS
+                or c < 0
+                or c == COLS
+                or grid[r][c] == 0
+                or (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
