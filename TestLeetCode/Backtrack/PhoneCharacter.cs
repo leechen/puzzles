5d0e00328311b103d4cc085bda82d71c52d4cbd0
Liqiang Chen <leeh.chen@gmail.com>
2024-07-14 13:32:37 -0700

    phone pad

---
+++ TestLeetCode/Backtrack/PhoneCharacter.cs
@@ -0,0 +1,34 @@
+
+
+public class LetterCombinationsSolution {
+    public IList<string> LetterCombinations(string digits) {
+        var res = new List<string>();
+        var digitToChar = new Dictionary<char, string> {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        void Backtrack(int i, string curStr) {
+            if (curStr.Length == digits.Length) {
+                res.Add(curStr);
+                return;
+            }
+            foreach (char c in digitToChar[digits[i]]) {
+                // The reason we don't need to remove or reduce i is that these are passed in by value
+                Backtrack(i + 1, curStr + c);
+            }
+        }
+
+        if (!string.IsNullOrEmpty(digits)) {
+            Backtrack(0, "");
+        }
+
+        return res;
+    }
+}
